816cb3cf33aaaca5ac888ae65cbb156e75715194
pjmantoss <joelptoss@gmail.com>
2020-12-16 09:38:23 +0100

    update jumbotron

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -12,3 +12,11 @@ export default function Jumbotron({children, direction = "row", ...restProps}){
 Jumbotron.Container = function JumbotronContainer({children, ...restProps}){
     return <Container {...restProps}>{children}</Container>
 }
+
+Jumbotron.Title = function JumbotronTitle({children, ...restProps}){
+    return <Title {...restProps}>{children}</Title>
+}
+
+Jumbotron.SubTitle = function JumbotronSubTitle({children, ...restProps}){
+    return <SubTitle {...restProps}>{children}</SubTitle>
+}
