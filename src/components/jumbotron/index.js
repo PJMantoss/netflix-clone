3b90f35e6ed376a4498866efd306b0029cb5f118
pjmantoss <joelptoss@gmail.com>
2020-12-16 08:55:13 +0100

    update jumbotron

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Inner, Item, Container } from './styles/jumbotron'
 
-export default function Jumbotron({}){
+export default function Jumbotron({children, direction = "row", ...restProps}){
     return(
         <Item>
             <Inner></Inner>
