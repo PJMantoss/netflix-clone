9c67df26c7619d3e29ffa7a78b57ca81f4308429
pjmantoss <joelptoss@gmail.com>
2020-12-16 09:11:45 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <Home />
+      <Switch>
+        <Route path="/">
+            <Home />
+        </Route>
+        <Route path="/sign-in">
+            <h2>Sign In Page</h2>
+        </Route>
+        <Route path="/sign-up">
+            <h2>Sign Up Page</h2>
+        </Route>
+      </Switch>
     </Router>
   );
 }
