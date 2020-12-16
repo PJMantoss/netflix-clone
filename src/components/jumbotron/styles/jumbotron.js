b0c2e2b5300df510ab4c1df3df211e2503d3e7d0
pjmantoss <joelptoss@gmail.com>
2020-12-16 12:55:57 +0100

    update jumbotron styling

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -14,11 +14,13 @@ export const Inner = styled.div`
     }
 `;
 
-export const Pane =styled.div`
+export const Pane = styled.div`
     width: 50%;
 
     @media (max-width: 1000px){
         width: 100%;
+        padding: 0 45px;
+        text-align: center;
     }
 `;
 
