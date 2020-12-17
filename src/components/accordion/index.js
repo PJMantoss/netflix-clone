53dda705f09bcd308ed5ea32c99e0f057fa2a7ae
pjmantoss <joelptoss@gmail.com>
2020-12-17 17:08:53 +0100

    update accordion

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,2 +1,10 @@
 import React, { useState } from 'react';
 import { Container, Inner, Body, Title, Header } from './styles/accordion';
+
+export default function Accordion({ cildren, ...restProps }){
+    return (
+        <Container {}>
+            <Inner>{}</Inner>
+        </Container>
+    )
+}
