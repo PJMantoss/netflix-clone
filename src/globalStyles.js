2915a739df14d1dfafa44a783428a1d0e72c54e9
pjmantoss <joelptoss@gmail.com>
2020-12-17 15:00:41 +0100

    update globalStyles

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -3,6 +3,8 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyles = createGlobalStyle`
     html, body {
         font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        font-size: 16px;
+        margin: 0;
         background: #000000;
         color: #333333;
     }
