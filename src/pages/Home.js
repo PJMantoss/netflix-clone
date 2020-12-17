546ad81a2b219af3c8c6c25ab1ce8022166bf5eb
pjmantoss <joelptoss@gmail.com>
2020-12-17 17:20:03 +0100

    update accordion(faqs) container

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {JumbotronContainer} from '../containers/Jumbotron';
 import { FooterContainer } from '../containers/Footer';
+import { FaqsContainer } from './containers/FaqsContainer';
 
 export default function Home(){
     return(
