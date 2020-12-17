e892adb4c07514b2f130de6d61fc440f08789e6b
pjmantoss <joelptoss@gmail.com>
2020-12-17 15:24:00 +0100

    update footer container

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import { Footer } from './components';
+
+export function FooterContainer(){
+    return(
+        <Footer>
+            <p>Copyright 2020</p>
+        </Footer>
+    )
+}
