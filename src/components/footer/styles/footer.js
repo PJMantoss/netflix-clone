e751fc8c2adc84af3b36cdd099a96b18735e4578
pjmantoss <joelptoss@gmail.com>
2020-12-17 16:43:12 +0100

    update footer styling

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     display: flex;
+    max-width: 1000px;
+    flex-direction: column;
 `;
 
 export const Row = styled.div``;
