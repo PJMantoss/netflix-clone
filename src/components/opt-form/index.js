12a3d573fd612b14b22d2c2e1b60cdb7a314ffbe
pjmantoss <joelptoss@gmail.com>
2020-12-18 19:28:15 +0100

    update opt-form

--- src/components/opt-form/index.js
+++ src/components/opt-form/index.js
@@ -5,6 +5,10 @@ export function OptForm({children, ...restProps}){
     return <Container {...restProps}>{children}</Container>
 }
 
-OptForm.Input = function OptFormInput({}){
-    return <Input />
+OptForm.Input = function OptFormInput({ ...restProps }){
+    return <Input {...restProps} />
+}
+
+OptForm.Button = function OptFormButton({}){
+    return <Button></Button>
 }
