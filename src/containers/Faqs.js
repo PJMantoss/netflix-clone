fb6242916f7966ac56775238e56f4365cc922954
pjmantoss <joelptoss@gmail.com>
2020-12-18 16:25:27 +0100

    update accordion styling

--- src/containers/Faqs.js
+++ src/containers/Faqs.js
@@ -6,7 +6,7 @@ export function FaqsContainer(){
     return(
         <Accordion>
             <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-            {FaqsData.map(item => (
+            {FaqsData.map((item) => (
                 <Accordion.Item key={item.id}>
                     <Accordion.Header>{item.header}</Accordion.Header>
                     <Accordion.Body>{item.body}</Accordion.Body>
