e757519adc93531e499f7292e0d10cc6be149226
pjmantoss <joelptoss@gmail.com>
2020-12-18 19:33:12 +0100

    update opt-form

--- src/components/opt-form/index.js
+++ src/components/opt-form/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  } from './styles/opt-form';
+import { Container, Input, Button, Text } from './styles/opt-form';
 
 export function OptForm({ children, ...restProps }){
     return <Container {...restProps}>{children}</Container>
