5a19ef396b47f3014e500e67a36821c6a6a44bb5
pjmantoss <joelptoss@gmail.com>
2020-12-19 19:40:58 +0100

    update Browse

--- src/containers/Browse.js
+++ src/containers/Browse.js
@@ -3,3 +3,4 @@ import { Header } from '../components';
 import * as ROUTES from './constants/routes';
 import { FirebaseContext } from './context/firebase';
 import { FooterContainer } from '../containers/Footer';
+import { selectProfileContainer } from './Profiles';
