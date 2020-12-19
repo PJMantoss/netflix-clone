21b28a98ca33661ff9336119da0609b10c7c0413
pjmantoss <joelptoss@gmail.com>
2020-12-19 09:05:46 +0100

    update header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Link as ReachRouterLink} from 'react-router-dom';
 import {  } from './styles/header';
 
-export function Header({ to="", ...restProps }){
-    return()
+export default function Header({ bg = true, children, ...restProps }){
+    return(
+        bg ? <Background {...restProps}>{children}</Background> : children
+    )
 }
