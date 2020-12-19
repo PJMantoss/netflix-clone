ad802139798f9c16b80885fe154591942b2154a6
pjmantoss <joelptoss@gmail.com>
2020-12-19 09:02:34 +0100

    update header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,2 +1,7 @@
 import React from 'react';
+import {Link as ReachRouterLink} from 'react-router-dom';
 import {  } from './styles/header';
+
+export function Header({ to="", ...restProps }){
+    return()
+}
