9510943423a729e4a3d52128c77e58395e4a6021
pjmantoss <joelptoss@gmail.com>
2020-12-19 14:26:24 +0100

    add/update Feature component

--- src/components/feature/styles/feature.js
+++ src/components/feature/styles/feature.js
@@ -4,6 +4,8 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     border-bottom: 8px solid #222;
+    text-align: center;
+    padding: 165px 45px;
 `;
 
 export const Title = styled.h1``;
