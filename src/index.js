499a7e5cd8329f6cb3141fe8426d96bc00483478
pjmantoss <joelptoss@gmail.com>
2020-12-19 14:50:56 +0100

    import firebase.js into index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { GlobalStyles } from './globalStyles'
+import { GlobalStyles } from './globalStyles';
+import { FirebaseContext } from './context/firebase';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
