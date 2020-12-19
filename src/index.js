feca22b0885888c5582e94d68ff9a1ff3e022d4e
pjmantoss <joelptoss@gmail.com>
2020-12-19 15:26:06 +0100

    add firebase config details

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import { FirebaseContext } from './context/firebase';
 import reportWebVitals from './reportWebVitals';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+    apiKey: "AIzaSyDfowjadOlRQoqWGOVC6jjbhWuy31fivyc",
+    authDomain: "netflix-clone-22d37.firebaseapp.com",
+    databaseURL: "https://netflix-clone-22d37-default-rtdb.firebaseio.com",
+    projectId: "netflix-clone-22d37",
+    storageBucket: "netflix-clone-22d37.appspot.com",
+    messagingSenderId: "407949998477",
+    appId: "1:407949998477:web:bb9a9276cdacd66de6a5c4"
 }
 
 const firebase = window.firebase.initializeApp(config);
