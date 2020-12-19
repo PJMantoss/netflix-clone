63e9131ccf3bac7e043a835da252585b38c40286
pjmantoss <joelptoss@gmail.com>
2020-12-19 17:57:21 +0100

    update Signin page

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -4,11 +4,21 @@ import { HeaderContainer } from '../containers/Header';
 
 export default function Signin(){
     const [error, setError] = useState('');
+
+    const handleChange = () => {
+        //call in firebase to handle authentication
+        //if there's an error, populate the error state
+    }
+
     return(
         <HeaderContainer>
             <Form>
                 <Form.Title>Sign In</Form.Title>
                 {error && <Form.Error>{error}</Form.Error>}
+
+                <Form.Base onSubmit={}>
+                    <Form.Input></Form.Input>
+                </Form.Base>
             </Form>
         </HeaderContainer>
     )
