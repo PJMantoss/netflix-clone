fb8be64d9dfa3754609897db0132659495ac9aa7
pjmantoss <joelptoss@gmail.com>
2020-12-19 09:20:32 +0100

    update Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,15 @@ import React from 'react';
 import {JumbotronContainer} from '../containers/Jumbotron';
 import { FooterContainer } from '../containers/Footer';
 import { FaqsContainer } from '../containers/Faqs';
+import { HeaderContainer } from '../containers/Header';
 
 export default function Home(){
     return(
         <>
+            <HeaderContainer>
+                <p>Hello</p>
+            </HeaderContainer>
+
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
