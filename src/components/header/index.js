91466cc3460e3df2dca669a86b7c61f5e4b1430a
pjmantoss <joelptoss@gmail.com>
2020-12-19 09:22:30 +0100

    update header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link as ReachRouterLink} from 'react-router-dom';
-import {  } from './styles/header';
+import { Background, Container, Frame, Logo, ButtonLink } from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }){
     return(
