ff9bc1ebc85a16b3aabc0ae773a9194ac5816461
pjmantoss <joelptoss@gmail.com>
2020-12-19 17:39:43 +0100

    update form component styling

--- src/components/form/styles/form.js
+++ src/components/form/styles/form.js
@@ -2,12 +2,16 @@ import styled from 'styled-components';
 
 export const Container = styled.div``;
 
+export const Base = styled.form``;
+
 export const Input = styled.input``;
 
 export const Title = styled.h1``;
 
-export const Text = styled.p``;
+export const Text = styled.h1``;
 
-export const TextSmall = styled.span``;
+export const TextSmall = styled.p``;
 
 export const Submit = styled.button``;
+
+export const Error = styled.div``;
