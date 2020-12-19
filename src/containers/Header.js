33ea5dcc726400d62eebcc2f0547465f907418bf
pjmantoss <joelptoss@gmail.com>
2020-12-19 09:16:03 +0100

    update header container

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,3 +1,12 @@
 import React from "react";
 import { Header } from '../components';
 import * as ROUTES from '../constants/routes';
+
+export function HeaderContainer({}){
+    return(
+        <Header>
+            <Header.Frame></Header.Frame>
+            {children}
+        </Header>
+    )
+}
