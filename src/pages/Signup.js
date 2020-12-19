c7039c6913ac782eefb7255da21d3fc0af3c5422
pjmantoss <joelptoss@gmail.com>
2020-12-19 19:05:17 +0100

    update Signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,3 +2,5 @@ import React from 'react';
 import { Form } from '../components';
 import { HeaderContainer } from '../containers/Header';
 import { FooterContainer } from '../containers/Footer';
+
+export default function Signup(){}
