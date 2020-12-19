e0c8f4efb852f3d48e279b339cb9cd9f8854fb0d
pjmantoss <joelptoss@gmail.com>
2020-12-19 19:39:38 +0100

    and Browse and Profiles containers

---
+++ src/containers/Browse.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { Header } from '../components';
+import * as ROUTES from './constants/routes';
+import { FirebaseContext } from './context/firebase';
+import { FooterContainer } from '../containers/Footer';
