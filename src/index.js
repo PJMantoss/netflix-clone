92b834c0d1b831263ca1ff72c9654b211524543c
pjmantoss <joelptoss@gmail.com>
2020-12-19 14:55:59 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ import reportWebVitals from './reportWebVitals';
 const config = {
   apiKey: '',
   authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
   
 }
 
