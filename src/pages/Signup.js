1c7f6dbaaddb1f5978fd559f20b0a3d3c879332d
pjmantoss <joelptoss@gmail.com>
2020-12-19 19:25:56 +0100

    update Signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -6,10 +6,10 @@ import * as ROUTES from '../constants/routes';
 
 export default function Signup(){
     //initialize states
-    const [firstName, setFirstName] = useSate('');
-    const [emailAddress, setEmailAddress] = useSate('');
-    const [password, setPassword] = useSate('');
-    const [error, setError] = useSate('');
+    const [firstName, setFirstName] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
     //form validation
     const isInvalid = firstName === '' | emailAddress === '' | password === '';
@@ -21,7 +21,49 @@ export default function Signup(){
 
     return(
         <>
-            <HeaderContainer></HeaderContainer>
+        <HeaderContainer>
+            <Form>
+                <Form.Title>Sign Up</Form.Title>
+                {error && <Form.Error>{error}</Form.Error>}
+
+                <Form.Base onSubmit={handleSignup} method="POST">
+                    <Form.Input
+                        type="text" 
+                        onChange={({ target }) => setFirstName(target.value)} 
+                        value={firstName} 
+                        placeholder="First Name" 
+                    />
+
+                    <Form.Input
+                        type="email" 
+                        onChange={({ target }) => setEmailAddress(target.value)} 
+                        value={emailAddress} 
+                        placeholder="E-mail Address" 
+                    />
+
+                    <Form.Input
+                        type="password" 
+                        onChange={({ target }) => setPassword(target.value)} 
+                        value={password} 
+                        autocomplete="off"
+                        placeholder="Password" 
+                    />
+
+                    <Form.Submit disabled={isInvalid} type="submit">
+                        Sign In
+                    </Form.Submit>
+
+                    <Form.Text>
+                        Already a user? <Form.Link to="/signin">Please Sign</Form.Link>
+                    </Form.Text>
+
+                    <Form.TextSmall>
+                        This page is protected by Google reCAPTCHA.
+                    </Form.TextSmall>
+                </Form.Base>
+            </Form>
+        </HeaderContainer>
+        <FooterContainer />
         </>
     )
 }
