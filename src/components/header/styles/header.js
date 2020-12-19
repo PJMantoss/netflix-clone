ff39bbc56dcce09f9f43b1ddc2c4884c5ad4cdf4
pjmantoss <joelptoss@gmail.com>
2020-12-19 09:24:35 +0100

    update header styling

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
+import { Link as ReachRouterLink } from 'react-router-dom';
 
 export const Background = styled.section``;
 
 export const Container = styled.div``;
 
-export const 
+export const Link = styled.p``;
