cc68a521f76d82c9e6ef238c6d49f434c3315c5e
pjmantoss <joelptoss@gmail.com>
2020-12-19 17:48:50 +0100

    update Signin page

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,1 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form } from '../components';
+import { HeaderContainer } from '../containers/Header';
+
+export default function Signin(){
+    const [] = useState('');
+    return(
+        <HeaderContainer>
+            <Form>
+                <Form.Title></Form.Title>
+                {error && <Form.Error>{error}</Form.Error>}
+            </Form>
+        </HeaderContainer>
+    )
+}
