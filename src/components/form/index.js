3c28decab734652ac9424769debb3adef13bb356
pjmantoss <joelptoss@gmail.com>
2020-12-19 16:42:40 +0100

    update form component

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import {  } from './styles/form';
 
-export default function Form({}){}
+export default function Form({ children, ...restProps }){
+    return <Container {...restProps}>{children}</Container>
+}
