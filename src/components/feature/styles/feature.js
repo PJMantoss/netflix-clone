accd5d7513032d56d05de0c3362483479c21a9ea
pjmantoss <joelptoss@gmail.com>
2020-12-19 14:25:56 +0100

    update Home

--- src/components/feature/styles/feature.js
+++ src/components/feature/styles/feature.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    border-bottom: 8px solid #222;
+`;
 
 export const Title = styled.h1``;
 
