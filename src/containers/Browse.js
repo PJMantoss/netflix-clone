a1815f9969dbf93699a52b5ac9b867ed8f306ace
pjmantoss <joelptoss@gmail.com>
2020-12-20 20:30:35 +0100

    update Browse

--- src/containers/Browse.js
+++ src/containers/Browse.js
@@ -22,7 +22,7 @@ export function BrowseContainer(){
     useEffect(() => {
         setTimeout(() => {
             setLoading(false)
-        }, 10000);
+        }, 4000);
     }, [user])
 
     return profile.displayName ? (
