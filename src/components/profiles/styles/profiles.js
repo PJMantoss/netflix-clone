51bf57d311a2741c4b36ad74aefbf656bfcbef8a
pjmantoss <joelptoss@gmail.com>
2020-12-20 08:59:36 +0100

    update Profiles component styling

--- src/components/profiles/styles/profiles.js
+++ src/components/profiles/styles/profiles.js
@@ -4,8 +4,12 @@ export const Container = styled.div``;
 
 export const Title = styled.h1``;
 
+export const User = styled.div``;
+
 export const List = styled.ul``;
 
 export const Item = styled.li``;
 
-export const User = styled.div``;
+export const Name = styled.p``;
+
+export const Picture = styled.img``;
