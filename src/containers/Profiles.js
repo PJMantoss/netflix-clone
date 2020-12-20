3269a4fa3840d964739c545c907a2fb55ee22476
pjmantoss <joelptoss@gmail.com>
2020-12-20 08:28:13 +0100

    update Profiles container

--- src/containers/Profiles.js
+++ src/containers/Profiles.js
@@ -1,2 +1,18 @@
 import React from 'react';
+import { Header, Profiles } from '../components';
 import * as ROUTES from './constants/routes';
+
+
+export function SelectProfileContainer({}){
+    return(
+        <>
+            <Header bg={false}>
+                <Header.Frame>
+                    <Header.Logo></Header.Logo>
+                </Header.Frame>
+            </Header>
+
+            <Profiles></Profiles>
+        </>
+    )
+}
