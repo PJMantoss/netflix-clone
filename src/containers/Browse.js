db445e42073e89d301328ed5b77573d414f73a76
pjmantoss <joelptoss@gmail.com>
2020-12-20 16:35:32 +0100

    update Browser.js

--- src/containers/Browse.js
+++ src/containers/Browse.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Header } from '../components';
 import * as ROUTES from '../constants/routes';
 import { FirebaseContext } from '../context/firebase';
@@ -6,7 +6,10 @@ import { FooterContainer } from './Footer';
 import { SelectProfileContainer } from './Profiles';
 
 export function BrowseContainer(){
+    //initialize state
+    const [category, setCategory] = useState(series)
     const [profile, setProfile] = useState({});
+    const [loading, setLoading] = useState(true);
 
     const user = {
         displayName: "Carl",
