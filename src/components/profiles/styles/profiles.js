956b205737fef8a568908ed9b98fb2fe86941875
pjmantoss <joelptoss@gmail.com>
2020-12-20 09:31:37 +0100

    update Profiles component styling

--- src/components/profiles/styles/profiles.js
+++ src/components/profiles/styles/profiles.js
@@ -4,6 +4,9 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
+    align-items: center;
+    margin: auto;
+    max-width: 80%;
 `;
 
 export const Title = styled.h1``;
