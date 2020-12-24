73bf7d5736adb087afaac184613834af9a9f66c2
pjmantoss <joelptoss@gmail.com>
2020-12-24 17:50:27 +0100

    update Player component styling

--- src/components/player/styles/player.js
+++ src/components/player/styles/player.js
@@ -2,7 +2,15 @@ import styled from 'styled-components';
 
 export const Container = styled.div``;
 
-export const Overlay = styled.div``;
+export const Overlay = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+
+`;
 
 export const Inner = styled.div``;
 
