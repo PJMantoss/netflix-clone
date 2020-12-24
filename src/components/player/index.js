9c6b798bafa88acc40fb761cd45cd8e516e786e0
pjmantoss <joelptoss@gmail.com>
2020-12-24 17:17:10 +0100

    update Player component styling

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -2,6 +2,27 @@ import React, { useState, useContext, createContext } from 'react';
 import ReactDOM from 'react-dom';
 import {} from './styles/player';
 
+const PlayerContext = createContext();
+
 export default function Player({ children, ...restProps }){
-    return <Container {...restProps}>{children}</Container>
+    const [showPlayer, setShowPlayer] = useState(false);
+
+    return(
+        <PlayerContext.Provider>
+            <Container {...restProps}>{children}</Container>
+        </PlayerContext.Provider>
+    )
+}
+
+Player.Video = function PlayerVideo({ ...restProps }){
+    const { showPlayer, setShowPlayer } = useContext(PlayerContext);
+
+    return showPlayer 
+        ? ReactDOM.createPortal(<Overlay onClick={() => setShowPlayer(false)}>
+            <Inner>
+                <video id="netflix-player" controls>
+                    <source src="./videos/bunny.mp4" type="video/mp4" />
+                </video>
+            </Inner>
+        </Overlay>, document.body) : null;
 }
